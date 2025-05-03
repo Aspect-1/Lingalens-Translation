b704ccf146029bce1e03920a69a626ee040e9f21
user.name "Aritra Bag <user.email "aritrabag5427@gmail.com>
2025-05-03 13:47:37 +0530

    update

--- app.py
+++ app.py
@@ -305,5 +305,5 @@ def process_image():
 
 if __name__ == '__main__':
     # For production, use a proper WSGI server instead of the built-in Flask server
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
